9f5e020438212d11875618d7d0ed4c494133e8ea
neuromancer <chenyifan_1997@hotmail.com>
2017-02-05 22:44:58 +0000

    using self-defined data types Coord and Tile; skeleton for interaction design

--- sokoban.hs
+++ sokoban.hs
@@ -3,7 +3,12 @@
 
 import CodeWorld
 
-data Tile = Wall | Ground | Storage | Box | Blank
+data Tile
+  = Wall
+  | Ground
+  | Storage
+  | Box
+  | Blank
 
 cube :: Double -> Picture
 cube a = solidRectangle a a
@@ -21,14 +26,14 @@ box :: Picture
 box = colored (dark brown) (cube 0.9) & ground
 
 drawTile :: Tile -> Picture
-drawTile Wall    = wall
-drawTile Ground  = ground
+drawTile Wall = wall
+drawTile Ground = ground
 drawTile Storage = storage
-drawTile Box     = box
-drawTile Blank   = blank
+drawTile Box = box
+drawTile Blank = blank
 
-maze :: Integer -> Integer -> Tile
-maze x y
+maze :: Coord -> Tile
+maze (C x y)
   | abs x > 4 || abs y > 4 = Blank
   | abs x == 4 || abs y == 4 = Wall
   | x == 2 && y <= 0 = Wall
@@ -42,12 +47,42 @@ pictureOfMaze = putCol (-10)
     putCol :: Integer -> Picture
     putCol x
       | x == 11 = blank
-      | otherwise = putBlock x (-10) & putCol (x + 1)
-    putBlock :: Integer -> Integer -> Picture
-    putBlock x y
+      | otherwise = putBlock (C x (-10)) & putCol (x + 1)
+    putBlock :: Coord -> Picture
+    putBlock c@(C x y)
       | y == 11 = blank
-      | otherwise =
-        translated (fromInteger x) (fromInteger y) (drawTile (maze x y)) & putBlock x  (y + 1)
+      | otherwise = atCoord c (drawTile (maze c)) & putBlock (adjacentCoord U c)
+
+data Direction
+  = R
+  | L
+  | U
+  | D
+
+data Coord =
+  C Integer
+    Integer
+
+initialCoord :: Coord
+initialCoord = C 0 0
+
+atCoord :: Coord -> Picture -> Picture
+atCoord (C x y) = translated (fromInteger x) (fromInteger y)
+
+adjacentCoord :: Direction -> Coord -> Coord
+adjacentCoord R (C x y) = C (x + 1) y
+adjacentCoord L (C x y) = C (x - 1) y
+adjacentCoord U (C x y) = C x (y + 1)
+adjacentCoord D (C x y) = C x (y - 1)
 
 main :: IO ()
-main = drawingOf pictureOfMaze
+main = interactionOf initialCoord handleTime handleEvent drawState
+
+handleTime :: Double -> Coord -> Coord
+handleTime _ c = c -- TODO
+
+handleEvent :: Event -> Coord -> Coord
+handleEvent _ _ = initialCoord -- TODO
+
+drawState :: Coord -> Picture
+drawState _ = blank -- TODO
