6779176b52540f17c29b385989c80d8c00a3dfbc
neuromancer <chenyifan_1997@hotmail.com>
2017-02-11 22:24:19 +0000

    implement multi-levels in-code

--- sokoban.hs
+++ sokoban.hs
@@ -2,6 +2,7 @@
 
 import CodeWorld
 import Data.Text
+import Data.Maybe
 
 -- basic blocks
 data Tile
@@ -35,20 +36,48 @@ drawTile Box = box & ground
 drawTile Blank = blank
 drawTile Player = player D & ground
 
--- Maze Construct
+-- Self defined list-structure
 data List a
   = Empty
   | Entry a
           (List a)
   deriving (Eq)
 
-newtype Maze =
-  Maze (Coord -> Tile)
-
 mapList :: (a -> b) -> List a -> List b
 mapList _ Empty = Empty
 mapList f (Entry c cs) = Entry (f c) (mapList f cs)
 
+{-
+elemList
+  :: Eq a
+  => a -> List a -> Bool
+elemList _ Empty = False
+elemList x (Entry xx xs)
+  | x == xx = True
+  | otherwise = elemList x xs
+
+appendList :: List a -> List a -> List a
+appendList Empty ys = ys
+appendList (Entry x xs) ys = Entry x (appendList xs ys)
+
+listLength :: List a -> Integer
+listLength Empty = 0
+listLength (Entry _ xs) = 1 + listLength xs
+
+filterList :: (a -> Bool) -> List a -> List a
+filterList _ Empty = Empty
+filterList f (Entry x xs)
+  | f x = Entry x (filterList f xs)
+  | otherwise = filterList f xs
+-}
+nth :: List a -> Integer -> Maybe a
+nth Empty _ = Nothing
+nth (Entry x xs) n
+  | n <= 0 = Nothing
+  | n == 1 = Just x
+  | otherwise = nth xs (n - 1)
+
+-- Maze Construct
 pictureOfBoxes :: Maze -> List Coord -> Picture
 pictureOfBoxes mz = combine . mapList drawBox
   where
@@ -81,17 +110,6 @@ pictureOfMaze (Maze noBox) = putCol (-10)
       | otherwise = atCoord c (drawTile (noBox c)) & putBlock (adjacentCoord U c)
     putBlock Nowhere = blank
 
-maze :: Coord -> Tile
-maze (C x y)
-  | x == 0 && y == -1 = Player
-  | abs x > 4 || abs y > 4 = Blank
-  | abs x == 4 || abs y == 4 = Wall
-  | x == 2 && y <= 0 = Wall
-  | x == 3 && y <= 0 = Storage
-  | x >= -2 && y == 0 = Box
-  | otherwise = Ground
-maze Nowhere = Blank
-
 noBoxMaze :: Maze -> Maze
 noBoxMaze (Maze mz) = Maze mz'
   where
@@ -101,9 +119,10 @@ noBoxMaze (Maze mz) = Maze mz'
         Player -> Ground
         t -> t
 
-initialState :: Maze -> State
-initialState (Maze raw) = State initialPostion D initialBoxes
+initialState :: Integer -> State
+initialState n = State initialPostion D initialBoxes n
   where
+    raw = (\(Maze mz) -> mz) (nthMaze mazes n)
     initialBoxes :: List Coord
     initialBoxes = scan raw upleft
       where
@@ -135,7 +154,7 @@ data Direction
   | L
   | U
   | D
-  deriving Eq
+  deriving (Eq)
 
 data Coord
   = C Integer
@@ -183,6 +202,7 @@ data State =
   State Coord
         Direction
         (List Coord)
+        Integer
   deriving (Eq)
 
 data SSState world
@@ -228,20 +248,29 @@ withStartScreen (Interaction state0 timer handle draw) = Interaction state0' tim
 runInteractionOf :: Interaction s -> IO ()
 runInteractionOf (Interaction state0 timer handle draw) = interactionOf state0 timer handle draw
 
-handleEvent :: Maze -> Event -> State -> State
-handleEvent mz _ s@(State _ _ boxList)
+handleEvent :: Event -> State -> State
+handleEvent (KeyPress key) (State _ _ boxList level)
+  | winning mz boxList && key == " " && level < 8 = initialState (level + 1)
+  | winning mz boxList && key == " " && level == 8 = initialState 1
+  where
+    mz = noBoxMaze (nthMaze mazes level)
+handleEvent _ s@(State _ _ boxList level)
   | winning mz boxList = s
-handleEvent mz (KeyPress key) s
+  where
+    mz = noBoxMaze (nthMaze mazes level)
+handleEvent (KeyPress key) s@(State _ _ _ level)
   | key == "Right" = go mz R s
   | key == "Left" = go mz L s
   | key == "Up" = go mz U s
   | key == "Down" = go mz D s
-handleEvent _ _ s = s
+  where
+    mz = noBoxMaze (nthMaze mazes level)
+handleEvent _ s = s
 
 go :: Maze -> Direction -> State -> State
-go (Maze mz) d s@(State c _ boxList)
-  | isPush nextCoord curMaze = State nextCoord d (makeNewList nextCoord d boxList)
-  | isMove nextCoord curMaze = State nextCoord d boxList
+go (Maze mz) d s@(State c _ boxList level)
+  | isPush nextCoord curMaze = State nextCoord d (makeNewList nextCoord d boxList) level
+  | isMove nextCoord curMaze = State nextCoord d boxList level
   | otherwise = s
   where
     nextCoord = adjacentCoord d c
@@ -270,24 +299,24 @@ go (Maze mz) d s@(State c _ boxList)
       | pos == c' = Entry (adjacentCoord d' c') cs
       | otherwise = Entry c' (makeNewList pos d' cs)
 
-drawState :: Maze -> State -> Picture
-drawState m (State pos dir boxList) =
+drawState :: State -> Picture
+drawState (State pos dir boxList level) =
   winPhase & atCoord pos (player dir) & pictureOfBoxes m boxList & pictureOfMaze m
   where
+    m = noBoxMaze (nthMaze mazes level)
     winPhase =
       if winning m boxList
-        then hint "Press ESC to restart" & scaled 3 3 (text "Winning!") &
+        then hint "Press SPACE to continue" &
+             translated 0 (-1) (hint "or Press ESC to restart") &
+             scaled 3 3 (text "Winning!") &
              colored (translucent (gray 0.5)) (cube 21)
         else blank
 
-basicInteraction :: Maze -> Interaction State
-basicInteraction m =
-  Interaction (initialState m) (\_ s -> s) (handleEvent (noBoxMaze m)) (drawState (noBoxMaze m))
+basicInteraction :: Integer -> Interaction State
+basicInteraction m = Interaction (initialState m) (\_ s -> s) handleEvent drawState
 
 main :: IO ()
-main =
-  let m = Maze maze
-  in (runInteractionOf . resetable . withStartScreen . withUndo) (basicInteraction m)
+main = (runInteractionOf . resetable . withStartScreen . withUndo) (basicInteraction 1)
 
 hint :: Text -> Picture
 hint t = translated 0 (-5) (text t)
@@ -315,3 +344,248 @@ withUndo (Interaction state0 timer handle draw) = Interaction state0' timer' han
       where
         s' = handle e s
     draw' (WithUndo s _) = draw s
+
+-- Mazes
+newtype Maze =
+  Maze (Coord -> Tile)
+
+mazes :: List Maze
+mazes =
+  Entry (Maze maze9) $
+  Entry (Maze maze8) $
+  Entry (Maze maze7) $
+  Entry (Maze maze6) $
+  Entry (Maze maze5) $ Entry (Maze maze4) $ Entry (Maze maze3) $ Entry (Maze maze1) Empty
+
+nthMaze :: List Maze -> Integer -> Maze
+nthMaze mazeList n = fromMaybe (Maze maze1) (nth mazeList n)
+
+{-
+extraMazes :: List Maze
+extraMazes = Entry (Maze maze4') $ Entry (Maze maze4'') $ Entry (Maze maze9') mazes
+-}
+maze1 :: Coord -> Tile
+maze1 (C x y)
+  | x == 0 && y == 1 = Player
+  | abs x > 4 || abs y > 4 = Blank
+  | abs x == 4 || abs y == 4 = Wall
+  | x == 2 && y <= 0 = Wall
+  | x == 3 && y <= 0 = Storage
+  | x >= -2 && y == 0 = Box
+  | otherwise = Ground
+maze1 Nowhere = Blank
+
+maze3 :: Coord -> Tile
+maze3 (C (-5) (-5)) = Wall
+maze3 (C (-5) (-4)) = Wall
+maze3 (C (-5) (-3)) = Wall
+maze3 (C (-5) (-2)) = Wall
+maze3 (C (-5) (-1)) = Wall
+maze3 (C (-5) 0) = Wall
+maze3 (C (-5) 1) = Wall
+maze3 (C (-5) 2) = Wall
+maze3 (C (-5) 3) = Wall
+maze3 (C (-5) 4) = Wall
+maze3 (C (-4) (-5)) = Wall
+maze3 (C (-4) (-4)) = Ground
+maze3 (C (-4) (-3)) = Ground
+maze3 (C (-4) (-2)) = Ground
+maze3 (C (-4) (-1)) = Ground
+maze3 (C (-4) 0) = Ground
+maze3 (C (-4) 1) = Ground
+maze3 (C (-4) 2) = Ground
+maze3 (C (-4) 3) = Player
+maze3 (C (-4) 4) = Wall
+maze3 (C (-3) (-5)) = Wall
+maze3 (C (-3) (-4)) = Ground
+maze3 (C (-3) (-3)) = Wall
+maze3 (C (-3) (-2)) = Wall
+maze3 (C (-3) (-1)) = Wall
+maze3 (C (-3) 0) = Wall
+maze3 (C (-3) 1) = Ground
+maze3 (C (-3) 2) = Wall
+maze3 (C (-3) 3) = Ground
+maze3 (C (-3) 4) = Wall
+maze3 (C (-3) 5) = Wall
+maze3 (C (-2) (-5)) = Wall
+maze3 (C (-2) (-4)) = Box
+maze3 (C (-2) (-3)) = Ground
+maze3 (C (-2) (-2)) = Ground
+maze3 (C (-2) (-1)) = Ground
+maze3 (C (-2) 0) = Wall
+maze3 (C (-2) 1) = Ground
+maze3 (C (-2) 2) = Box
+maze3 (C (-2) 3) = Box
+maze3 (C (-2) 4) = Ground
+maze3 (C (-2) 5) = Wall
+maze3 (C (-1) (-6)) = Wall
+maze3 (C (-1) (-5)) = Wall
+maze3 (C (-1) (-4)) = Ground
+maze3 (C (-1) (-3)) = Ground
+maze3 (C (-1) (-2)) = Ground
+maze3 (C (-1) (-1)) = Ground
+maze3 (C (-1) 0) = Wall
+maze3 (C (-1) 1) = Ground
+maze3 (C (-1) 2) = Ground
+maze3 (C (-1) 3) = Box
+maze3 (C (-1) 4) = Ground
+maze3 (C (-1) 5) = Wall
+maze3 (C (-1) 6) = Wall
+maze3 (C 0 (-6)) = Wall
+maze3 (C 0 (-5)) = Ground
+maze3 (C 0 (-4)) = Ground
+maze3 (C 0 (-3)) = Ground
+maze3 (C 0 (-2)) = Ground
+maze3 (C 0 (-1)) = Ground
+maze3 (C 0 0) = Wall
+maze3 (C 0 1) = Wall
+maze3 (C 0 2) = Wall
+maze3 (C 0 3) = Wall
+maze3 (C 0 4) = Ground
+maze3 (C 0 5) = Ground
+maze3 (C 0 6) = Wall
+maze3 (C 1 (-6)) = Wall
+maze3 (C 1 (-5)) = Ground
+maze3 (C 1 (-4)) = Ground
+maze3 (C 1 (-3)) = Ground
+maze3 (C 1 (-2)) = Ground
+maze3 (C 1 (-1)) = Ground
+maze3 (C 1 0) = Wall
+maze3 (C 1 1) = Storage
+maze3 (C 1 2) = Storage
+maze3 (C 1 3) = Storage
+maze3 (C 1 4) = Ground
+maze3 (C 1 5) = Ground
+maze3 (C 1 6) = Wall
+maze3 (C 2 (-6)) = Wall
+maze3 (C 2 (-5)) = Wall
+maze3 (C 2 (-4)) = Ground
+maze3 (C 2 (-3)) = Ground
+maze3 (C 2 (-2)) = Ground
+maze3 (C 2 (-1)) = Ground
+maze3 (C 2 0) = Wall
+maze3 (C 2 1) = Wall
+maze3 (C 2 2) = Wall
+maze3 (C 2 3) = Wall
+maze3 (C 2 4) = Wall
+maze3 (C 2 5) = Wall
+maze3 (C 2 6) = Wall
+maze3 (C 3 (-5)) = Wall
+maze3 (C 3 (-4)) = Ground
+maze3 (C 3 (-3)) = Ground
+maze3 (C 3 (-2)) = Storage
+maze3 (C 3 (-1)) = Ground
+maze3 (C 3 0) = Wall
+maze3 (C 4 (-5)) = Wall
+maze3 (C 4 (-4)) = Wall
+maze3 (C 4 (-3)) = Wall
+maze3 (C 4 (-2)) = Wall
+maze3 (C 4 (-1)) = Wall
+maze3 (C 4 0) = Wall
+maze3 _ = Blank
+
+maze4 :: Coord -> Tile
+maze4 (C x y)
+  | x == 1 && y == (-3) = Player
+  | abs x > 4 || abs y > 4 = Blank
+  | abs x == 4 || abs y == 4 = Wall
+  | x == 2 && y < 0 = Wall
+  | x >= -1 && y == 1 && x <= 2 = Wall
+  | x == -3 && y == 1 = Wall
+  | x == 0 && y == 3 = Wall
+  | x == 0 && y == 0 = Wall
+  | x == 3 && y == -3 = Storage
+  | x == 1 && y == 2 = Storage
+  | x == -3 && y == 2 = Storage
+  | x == 1 && y == -1 = Storage
+  | x == -2 && y == 1 = Box
+  | x == 2 && y == 2 = Box
+  | x <= 1 && y == -2 && x >= 0 = Box
+  | otherwise = Ground
+maze4 Nowhere = Blank
+
+maze5 :: Coord -> Tile
+maze5 (C x y)
+  | x == 0 && y == 1 = Player
+  | abs x > 4 || abs y > 4 = Blank
+  | abs x == 4 || abs y == 4 = Wall
+  | x == 1 && y < 0 = Wall
+  | x == -3 && y == -2 = Wall
+  | x <= 1 && x > -2 && y == 0 = Wall
+  | x > -3 && x < 3 && y == 2 = Wall
+  | x == 3 && y > 1 = Storage
+  | y == -2 && x < 0 = Box
+  | y == -2 && x == 2 = Box
+  | y == 0 && x == 3 = Box
+  | y == -1 && x > 1 && x < 4 = Storage
+  | otherwise = Ground
+maze5 Nowhere = Blank
+
+maze6 :: Coord -> Tile
+maze6 (C x y)
+  | x == (-2) && y == 4 = Player
+  | abs x > 3 || abs y > 5 = Blank
+  | abs x == 3 || (abs y == 5 && abs x < 4) = Wall
+  | x == 0 && abs y < 4 = Storage
+  | x == -1 && (y == 0 || abs y == 2) = Box
+  | x == 1 && (abs y == 1 || abs y == 3) = Box
+  | x == (-2) && y == 1 = Wall
+  | otherwise = Ground
+maze6 Nowhere = Blank
+
+maze7 :: Coord -> Tile
+maze7 (C x y)
+  | x == (-3) && y == 3 = Player
+  | abs x > 4 || abs y > 4 = Blank
+  | abs x == 4 || abs y == 4 = Wall
+  | x /= 2 && y == 2 = Wall
+  | x /= -2 && y == -1 = Wall
+  | x == 3 && y == -3 = Storage
+  | x == 2 && y == 2 = Box
+  | otherwise = Ground
+maze7 Nowhere = Blank
+
+maze8 :: Coord -> Tile
+maze8 (C x y)
+  | x == 0 && y == 0 = Player
+  | abs x > 10 || abs y > 10 = Blank
+  | x == 0 && y == 0 = Ground
+  | abs x == 9 && abs y == 9 = Wall
+  | abs x == 10 || abs y == 10 = Wall
+  | x == y = Storage
+  | abs x == abs y = Box
+  | x < 0 && x > (-9) && y == 0 = Box
+  | x > 0 && x < 9 && y == 0 = Storage
+  | otherwise = Ground
+maze8 Nowhere = Blank
+
+maze9 :: Coord -> Tile
+maze9 (C x y)
+  | x == 1 && y == 1 = Player
+  | abs x > 4 || abs y > 4 = Blank
+  | abs x == 4 || abs y == 4 || x == -3 = Wall
+  | x == -2 && (y == 3 || y == 0) = Wall
+  | x == -1 && y == -1 = Wall
+  | x == -0 && y == 1 = Wall
+  | x == 3 && y == 0 = Wall
+  | x < 0 && (y == 2 || y == -3) = Storage
+  | x == -1 && y == 1 = Storage
+  | x == 0 && (y == 2 || y == 0 || y == -1) = Box
+  | x == 1 && y == -2 = Box
+  | x == 2 && y == -3 = Box
+  | otherwise = Ground
+maze9 Nowhere = Blank
+{-
+maze4'' :: Coord -> Tile
+maze4'' (C 1 (-3)) = Box
+maze4'' c = maze4 c
+
+maze4' :: Coord -> Tile
+maze4' (C 0 1) = Blank
+maze4' c = maze4 c
+
+maze9' :: Coord -> Tile
+maze9' (C 3 0) = Box
+maze9' (C 4 0) = Box
+maze9' c = maze9 c
+-}
